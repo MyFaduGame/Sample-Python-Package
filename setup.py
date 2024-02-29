4775d1e04205c81c544fcc66665996791a2f7051
Navin319931 <navinsharma9376319931@gmail.com>
2024-02-29 23:34:15 +0530

    added new factor

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "sample_pkg",
+    name = "trial_repo",
     version = "0.0.1",
     author = "MyFaduGame",
     author_email = "navinsharma9376319931@gmail.com",
     description = "This is an sample Package to build it only contains the sample multiply 3 structure",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = "package URL",
+    url = "https://www.github.com/MyFaduGame/",
     project_urls = {
-        "Bug Tracker": "package issues URL",
+        "Bug Tracker": "https://www.github.com/MyFaduGame/",
     },
     classifiers = [
         "Programming Language :: Python :: 3",
